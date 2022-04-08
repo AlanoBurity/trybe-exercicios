fa3e9c6bee14983b431d2e719787bd642a15005b
AlanoBurity <alanosantosdev@gmail.com>
2022-04-07 22:07:07 -0300

    exercicios de 1 a 6

---
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-2.js
@@ -0,0 +1,12 @@
+const valor1 = 90000;
+const valor2 = 6;
+
+if (valor1 > valor2){
+   console.log("O valor1 é maior que o valor2");
+}
+else if (valor2 > valor1){
+    console.log("O valor2 é maior que o valor1");
+}
+else {
+    console.log("Os numeros são iguais");
+}
---
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operações-aritimeticas/subtração.js
@@ -0,0 +1,5 @@
+let a = 100;
+let b = 77;
+
+
+console.log(a - b);
