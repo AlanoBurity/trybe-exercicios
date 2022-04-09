c61f5e561123a00dba323593ea2b13a51e727692
AlanoBurity <alanosantosdev@gmail.com>
2022-04-09 14:20:52 -0300

    exercicio  8 9 10

---
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-9.js
@@ -0,0 +1,14 @@
+// Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar.
+// Caso contrário, ele retorna false .
+// Bonus: use somente um if .
+
+const number1 =4 ;
+const number2 = 2 ;
+const number3 = 14 ;
+
+ if ( number1 % 2 !== 0 ||  number2 % 2 !== 0 ||  number3 % 2 !== 0 ){
+
+    console.log("true");
+ }else {
+    console.log("false");
+ }
