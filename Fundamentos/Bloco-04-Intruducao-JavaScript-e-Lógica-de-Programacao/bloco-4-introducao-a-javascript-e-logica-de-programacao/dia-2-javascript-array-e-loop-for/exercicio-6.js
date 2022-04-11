495c14a648d3922ed3034af48e82327a92742065
AlanoBurity <alanosantosdev@gmail.com>
2022-04-10 22:23:53 -0300

    tester exercicio 6 e 7

--- Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
@@ -3,8 +3,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let impar =[];
 
 for (let i = 0 ; i < numbers.length ; i += 1){
-    if (( numbers[i] % 2) !== 0 ) {
+    if (( numbers[i] % 2) != 0 ) {
       impar.push(i);
+    } else {
+      console.log("nenhum valor ímpar encontrado");
     }
   
 }
--- Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-7.js
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-7.js
@@ -1,10 +1,12 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = ;
+
+let menor = 0 ;
 
 for (let i = 0 ; i < numbers.length ; i += 1){
+    
     if ( numbers [i] < menor) {
-        menor = numbers[i];
+       menor = numbers[i];
     }
   
 }
