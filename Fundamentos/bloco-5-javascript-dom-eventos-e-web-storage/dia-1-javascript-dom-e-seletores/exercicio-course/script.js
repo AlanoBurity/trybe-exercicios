f336e893e73a9d7f18183bcf939067cf0a9782b2
AlanoBurity <alanosantosdev@gmail.com>
2022-04-17 00:44:22 -0300

    exercicio do course não obrigatorio

---
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio-course/script.js
@@ -0,0 +1,16 @@
+
+
+
+document.getElementById("header-container").style.backgroundColor = "#00b068"
+document.getElementById("header-container").style.border = "3px black solid"
+
+document.querySelectorAll("h3")[0].style.backgroundColor = "#a500f4"
+document.querySelectorAll("h3")[1].style.backgroundColor = "#a500f4"
+
+document.querySelectorAll("h3")[2].style.backgroundColor = "#232524"
+document.querySelectorAll("h3")[3].style.backgroundColor = "#232524"
+
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = "#ff9f84"
+document.querySelectorAll(".no-emergency-tasks")[0].style.backgroundColor = "#f9dc5d"
+
+document.getElementById("footer-container").style.backgroundColor = "#013533"
