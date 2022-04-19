851f6ae72476c349fe4553ef7e50b9f583633794
AlanoBurity <alanosantosdev@gmail.com>
2022-04-18 22:15:14 -0300

    Exercicio 1!

--- Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -15,3 +15,25 @@ function createDaysOfTheWeek() {
   
   // Escreva seu código abaixo.
   const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  
+
+  let diasDoMes =  document.querySelector('#days');
+
+  for( let index = 0; index < dezDaysList.length; index +=1){
+    let dias = dezDaysList[index]; 
+  
+    let diasDoMesListItem = document.createElement('li');
+    diasDoMesListItem.innerHTML = dias;
+    diasDoMesListItem.className = 'day';
+
+    diasDoMes.appendChild(diasDoMesListItem);
+
+    if ( dias === 24 || dias === 25 || dias === 31){
+      diasDoMesListItem.className = 'day holiday'; 
+     
+    }
+    if ( dias === 4 || dias === 11 || dias === 18 || dias === 25){
+      diasDoMesListItem.className = 'day friday';
+    }
+    
+}
