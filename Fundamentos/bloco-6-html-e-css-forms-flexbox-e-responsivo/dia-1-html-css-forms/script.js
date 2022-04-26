1df65b9d11a191de434afdad89fceef884906dba
AlanoBurity <alanosantosdev@gmail.com>
2022-04-26 19:52:34 -0300

    ajustes depois da monitoria

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,15 @@
+const select = document.querySelector('#container');
+
+const btnErase = document.createElement('button')
+
+
+btnErase.id = "limpar";
+select.appendChild(btnErase);
+
+btnErase.innerText = "Apagar";
+
+btnErase.style.marginLeft = "170px";
+
+
+console.log(btn)
+
