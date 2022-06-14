498a0fadcbec052c78c812d6c799610746887807
Alano <alanosantosdev@gmail.com>
2022-06-13 23:51:24 -0300

    questao 1 a 4

---
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/questao4.js
@@ -0,0 +1,14 @@
+let name = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { golden: 2, silver: 3 };
+
+
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { golden: 2, silver: 3 },
+};
+player.bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018];
+console.log('A jogadora ' + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo por ' + player.bestInTheWorld.length + ' vezes.');
