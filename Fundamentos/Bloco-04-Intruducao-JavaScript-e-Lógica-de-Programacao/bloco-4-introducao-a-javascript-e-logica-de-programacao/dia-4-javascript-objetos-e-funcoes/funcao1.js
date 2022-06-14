c9cee7fc9da75ea021e145ebade17ecd64bd9900
Alano <alanosantosdev@gmail.com>
2022-06-13 23:54:06 -0300

    função 1

---
+++ Fundamentos/Bloco-04-Intruducao-JavaScript-e-Lógica-de-Programacao/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcao1.js
@@ -0,0 +1,27 @@
+/* 1 - Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+Adição (a + b)
+Subtração (a - b)
+Multiplicação (a * b)
+Divisão (a / b)
+Módulo (a % b) */
+
+function soma(a, b) {
+    return a + b;
+  }
+  
+  function sub(a, b) {
+    return a - b;
+  }
+  
+  function mult(a, b) {
+    return a * b;
+  }
+  
+  function div(a, b) {
+    return a / b;
+  }
+  
+  function mod(a, b) {
+    return a % b;
+  }
+  
