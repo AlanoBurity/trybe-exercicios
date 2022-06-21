de96548c908dac646a27f22c57187cb9e953e79d
Alano <alanosantosdev@gmail.com>
2022-06-21 08:57:25 -0300

    exercicio 1 e 2

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-react-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/primeiro-react-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+const compromissos = [ 'estudar', 'viajar', 'namorar', 'jogar' ];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+   { Task('React') }
+   { Task('JS') } 
+   { Task('CSS') }  
+   { Task('VASCO') } 
+   { compromissos.map((itens) => Task(itens)) }
+   </div>
   );
 }
+
 
 export default App;
