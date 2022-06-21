0854c08497b2eff2f46f5f5b6daa571b4e501c8d
Alano <alanosantosdev@gmail.com>
2022-06-21 09:48:47 -0300

    exercicio 3 feito

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/segundo-react-app/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/segundo-react-app/src/App.js
@@ -1,4 +1,5 @@
 import Header from './Header';
+import Content from './Content';
 import './App.css';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
      <div>
       <Header />
+      <Content />
      </div>
   );
 }
